ed3efa950c937e78f21b2e56361033daa3403229
Marty Ulrich <marty@martyulrich.com>
2019-06-19 15:10:52 -0700

    Initial Commit

---
+++ Serendipia/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Serendipia
+//
+//  Created by Marty Ulrich on 6/19/19.
+//  Copyright © 2019 Marty Ulrich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ Serendipia/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Serendipia
+//
+//  Created by Marty Ulrich on 6/19/19.
+//  Copyright © 2019 Marty Ulrich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
