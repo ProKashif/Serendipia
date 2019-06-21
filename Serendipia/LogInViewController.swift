084f6a61702b27fa466c56d891e2f06b6136c571
Marty Ulrich <marty@martyulrich.com>
2019-06-21 11:32:15 -0700

    Added launch screen and log in controller

---
+++ Serendipia/LogInViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartLandingController.swift
+//  Serendipia
+//
+//  Created by Marty Ulrich on 6/19/19.
+//  Copyright © 2019 Marty Ulrich. All rights reserved.
+//
+
+import UIKit
+
+class LogInController: UIViewController {
+	
+}
---
+++ Serendipia/ViewExtensions.swift
@@ -0,0 +1,135 @@
+import Foundation
+import UIKit
+
+//TODO: figure out why this won't work as an extension on UIView
+@IBDesignable class DesignableView: UIView {
+  
+    // Rounded corner raius
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    // Shadow color
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    // Shadow offsets
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    // Shadow opacity
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    // Shadow radius
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return self.layer.shadowRadius
+        }
+        set {
+            self.layer.shadowRadius = newValue
+        }
+    }
+    
+    // Border width
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    // Border color
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    // Background color
+    @IBInspectable var layerBackgroundColor: UIColor {
+        get {
+            return UIColor(cgColor: self.layer.backgroundColor!)
+        }
+        set {
+            self.backgroundColor = nil
+            self.layer.backgroundColor = newValue.cgColor
+        }
+    }
+  
+    // Create bezier path of shadow for rasterize
+    @IBInspectable var enableBezierPath: Bool {
+        get {
+            return self.layer.shadowPath != nil
+        }
+        set {
+            if enableBezierPath {
+                self.layer.shadowPath = UIBezierPath(roundedRect: self.layer.bounds, cornerRadius: self.layer.cornerRadius).cgPath
+            } else {
+                self.layer.shadowPath = nil
+            }
+        }
+    }
+    
+    // Mask to bounds controll
+    @IBInspectable var maskToBounds: Bool {
+        get{
+            return self.layer.masksToBounds
+        }
+        set {
+            self.layer.masksToBounds = newValue
+        }
+    }
+    
+    // Rasterize option
+    @IBInspectable var rasterize: Bool {
+        get {
+            return self.layer.shouldRasterize
+        }
+        set {
+            self.layer.shouldRasterize = newValue
+            self.layer.rasterizationScale = UIScreen.main.scale
+        }
+    }
+}
+
+
+@IBDesignable class DesignableButton: UIButton {
+	// Rounded corner raius
+	@IBInspectable var cornerRadius: CGFloat {
+		get {
+			return self.layer.cornerRadius
+		}
+		set {
+			self.layer.cornerRadius = newValue
+		}
+	}
+}
