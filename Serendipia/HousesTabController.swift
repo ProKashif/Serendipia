ded605e451f4fcc8fb3d026e8d66cb667333c3bc
Marty Ulrich <marty@martyulrich.com>
2019-06-21 23:41:11 -0700

    Added calendar, pods

--- Serendipia/HousesTabController.swift
+++ Serendipia/HousesTabController.swift
@@ -6,8 +6,49 @@
 //  Copyright © 2019 Marty Ulrich. All rights reserved.
 //
 
-import UIKit
+import PureLayout
 
 class HousesTabController: UIViewController {
+	private var calendarView: UIView?
 	
+	@IBOutlet weak var fromDateField: UITextField!
+	@IBOutlet weak var toDateField: UITextField!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		calendarView = storyboard?.instantiateViewController(withIdentifier: "calendar").view
+		fromDateField.inputView = calendarView
+		toDateField.inputView = calendarView
+		fromDateField.inputAccessoryView = makeCalendarInputAccessoryView(title: "From")
+		toDateField.inputAccessoryView = makeCalendarInputAccessoryView(title: "To")
+	}
+	
+	private func makeCalendarInputAccessoryView(title: String) -> UIView {
+		let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 60))
+		let titleLabel = UILabel()
+		titleLabel.text = title
+		let closeButton = UIButton()
+		closeButton.setImage(UIImage(named: "close"), for: .normal)
+		
+		view.addSubview(titleLabel)
+		view.addSubview(closeButton)
+		
+		titleLabel.autoPinEdges(toSuperviewMarginsExcludingEdge: .right)
+		closeButton.autoPinEdges(toSuperviewMarginsExcludingEdge: .left)
+		
+		closeButton.addTarget(self, action: #selector(close), for: .touchUpInside)
+		
+		view.backgroundColor = .white
+		
+		return view
+	}
+	
+	@IBAction func dismissFirstResponder(_ sender: Any) {
+		(view.firstResponder as? UITextField)?.endEditing(true)
+	}
+	
+	@objc private func close() {
+		fromDateField.endEditing(true)
+		toDateField.endEditing(true)
+	}
 }
--- Serendipia/ViewExtensions.swift
+++ Serendipia/ViewExtensions.swift
@@ -141,3 +141,18 @@ extension UIViewController {
 	}
 	
 }
+
+
+extension UIView {
+	var firstResponder: UIView? {
+		guard !isFirstResponder else { return self }
+		
+		for subview in subviews {
+			if let firstResponder = subview.firstResponder {
+				return firstResponder
+			}
+		}
+		
+		return nil
+	}
+}
